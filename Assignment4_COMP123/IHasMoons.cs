c67380f18f3af43fa5ef4007d015cf83fcb06d0e
chintan89 <chintan@MYPERSONAL>
2017-07-16 15:16:28 -0400

    Create Method header in all interface file 8.0 - Program Strcture Part Completed

--- Assignment4_COMP123/IHasMoons.cs
+++ Assignment4_COMP123/IHasMoons.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Name - Chintan Patel
+ * Student # 300622893
+ * Date - July 16/2017
+ * version - 0.1 Create Method Header
+ */
 namespace Assignment4_COMP123
 {
     interface IHasMoons
     {
+        /* 4.Create Method Header HasMoons that returns bool data*/
+        //bool HasMoons;
     }
 }
