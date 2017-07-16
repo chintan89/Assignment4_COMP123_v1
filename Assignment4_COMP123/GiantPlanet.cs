a5a3ae28557b365424a1a49d3798be2c53b440a8
chintan89 <chintan@MYPERSONAL>
2017-07-16 15:29:24 -0400

    Create Instant Variable in GiantPlanet & TerrestrialPlanet class.

--- Assignment4_COMP123/GiantPlanet.cs
+++ Assignment4_COMP123/GiantPlanet.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Name - Chintan Patel
+ * Student # 300622893
+ * Date - July 16/2017
+ * version - 0.1 
+ */
 namespace Assignment4_COMP123
 {
     class GiantPlanet
     {
+        //2a.Private instance variable
+        private string _type;
+
+        //GiantPlanet(string name, double diameter, double mass, string type) : base(name, diameter, mass)
+        //{
+
+        //}
     }
+
 }
--- Assignment4_COMP123/Terrestrialplanet.cs
+++ Assignment4_COMP123/Terrestrialplanet.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Name - Chintan Patel
+ * Student # 300622893
+ * Date - July 16/2017
+ * version - 0.1 Create Method Header
+ */
 namespace Assignment4_COMP123
 {
     class Terrestrialplanet
     {
+        //3a.Private instance variable
+        private bool _oxygen;
+
+        //TerrestrialPlanet(string name, double diameter, double mass, bool oxygen) : base(name, diameter, mass)
+        //{
+
+        //}
     }
 }
