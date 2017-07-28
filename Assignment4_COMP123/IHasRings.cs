d6bf5eefa7f2401eccb522c337c5c3672a9cf50f
chintan89 <chintan@MYPERSONAL>
2017-07-27 22:39:42 -0400

    Modified HasRings interface

--- Assignment4_COMP123/IHasRings.cs
+++ Assignment4_COMP123/IHasRings.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 /*
  * Name - Chintan Patel
  * Student # 300622893
- * Date - July 16/2017
- * version - 0.1 Create Method Header
+ * Date - July 27/2017
+ * version - 0.2 Modified HasRings interface that returns bool data
  */
 namespace Assignment4_COMP123
 {
     interface IHasRings
     {
         /* 5.Create Method Header HasRings that returns bool data*/
-        //bool HasRings;
+        bool HasRings();
     }
 }
