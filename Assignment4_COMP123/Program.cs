d5a847ecec714ea6368dd61fbbdab682bb4c4e34
chintan89 <chintan@MYPERSONAL>
2017-07-27 23:15:31 -0400

    Created objects and displayed the result, and implemented method WaitforAnyKey

--- Assignment4_COMP123/Program.cs
+++ Assignment4_COMP123/Program.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 /* 
  * Name - Chintan Patel
  * Student # 300622893
- * Date - July 16/2017
+ * Date - July 27/2017
+ * version 0.1 - Create objects and displayed the result, and implement method WaitforAnyKey
  */
 namespace Assignment4_COMP123
 {
@@ -14,6 +15,25 @@ namespace Assignment4_COMP123
     {
         static void Main(string[] args)
         {
+            //7a.Create a new GiantPlanet object
+            GiantPlanet giantPlanet = new GiantPlanet("Jupiter", 136.485, 1889.4, "Gas");
+
+            //7b.method to disply planet's stats to the console
+            giantPlanet.ToString();
+
+            //7c.Create a new Terrestrialplanet object
+            Terrestrialplanet terrestrialPlanet = new Terrestrialplanet("Earth", 11.998, 6.112, true);
+
+            //7d.method to disply planet's stats to the console
+            terrestrialPlanet.ToString();
+
+            //7e. WaitForAnyKey
+            Program.WaitforAnyKey();
+        }
+        //7e. Implemented another method for WaitforAnyKey
+        private static void WaitforAnyKey()
+        {
+            Console.ReadKey();
         }
     }
 }
