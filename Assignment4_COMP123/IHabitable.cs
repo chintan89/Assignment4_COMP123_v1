89778cb27db5cd4b4a7c0f78e549b930ce08096c
chintan89 <chintan@MYPERSONAL>
2017-07-27 22:36:32 -0400

    Modified Habitable interface class

--- Assignment4_COMP123/IHabitable.cs
+++ Assignment4_COMP123/IHabitable.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 /*
  * Name - Chintan Patel
  * Student # 300622893
- * Date - July 16/2017
- * version - 0.1 Create Method Header
+ * Date - July 27/2017
+ * version - 0.2 Modified Habitable interface that returns bool data
  */
 namespace Assignment4_COMP123
 {
     interface IHabitable
     {
     /* 6.Create Method Header Habitable that returns bool data*/
-        //bool Habitable;
+        bool Habitable();
     }
 }
